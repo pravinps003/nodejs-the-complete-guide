b20368204f8ced01dfd25ff362bf9fbc1db5f5fb
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-02 00:45:16 +0530

    different ways of exporting modules from a file

--- code/05-using-the-node-modules-system/routes.js
+++ code/05-using-the-node-modules-system/routes.js
@@ -55,4 +55,16 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-module.exports = requestHandler;
+/* Different ways of handling exports from a file */
+// module.exports = requestHandler;
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: 'Some hard coded text',
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some hard coded text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
