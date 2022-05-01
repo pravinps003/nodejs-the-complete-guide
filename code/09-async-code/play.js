9d9066473a791fd22b6cafda61bad217e02ae5c1
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 18:28:25 +0530

    added nested promises

--- code/09-async-code/play.js
+++ code/09-async-code/play.js
@@ -9,9 +9,14 @@ const fetchData = () => {
 
 setTimeout(() => {
   console.log('Timer is done!');
-  fetchData().then((text) => {
-    console.log(text);
-  });
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
 }, 2000);
 
 console.log('Hello');
