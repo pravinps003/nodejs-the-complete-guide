7edda5831426c12ec945ac8325cda2715c2e0106
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-02 00:09:24 +0530

    understanding synchronous and asynchronous execution
    understanding synchronous and asynchronous execution with fs.writeFileSync and fs.writeFile

---
+++ code/04-blocking-and-non-blocking-code/app.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write(
+      `
+        <html>
+        <head><title>Enter Message</title></head>
+        <body>
+            <form action="/message" method="POST">
+                <input type="text" name="message"/>
+                <button type="submit">Send</button>
+            </form>
+        </body>
+        </html>
+        `
+    );
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      /* fs.writeFileSync is a synchronous method whereas
+      fs.writeFile asynchronously writes data to a file, providing a callback */
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write(
+    `
+    <html>
+        <head><title>My First Page</title></head>
+        <body>
+            <h1>
+                Hello from my Node.js Server!
+            </h1>
+        </body>
+    </html>
+    `
+  );
+  res.end();
+});
+
+server.listen(3000);
