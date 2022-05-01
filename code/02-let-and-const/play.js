fb79621fe356b19c32a7bcb47546d82e004a3c7a
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 14:53:26 +0530

    reassigning const variable

--- code/02-let-and-const/play.js
+++ code/02-let-and-const/play.js
@@ -2,6 +2,10 @@ const name = 'PS003';
 let age = 29;
 const hasHobbies = true;
 
+/* Uncomment the below line and try it out */
+// name = 'Pravin';
+age = 30;
+
 function summarizeUser(userName, userAge, userHasHobby) {
   return (
     'Name is ' +
