66d5700c7ab6ecfee6f94bd26e34f024bb13ecdb
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 21:02:09 +0530

    reading specific request parameters

--- code/01-creating-a-node-server/app.js
+++ code/01-creating-a-node-server/app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
 
   /* The process core module provides a handy method that allows you to programmatically exit from a Node.js program */
-  process.exit();
+  // process.exit();
 });
 
 server.listen(3000);
