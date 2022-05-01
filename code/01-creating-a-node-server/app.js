5253a5d1cee100ecb9ca687c66e7958b2fbbf3db
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 20:58:34 +0530

    added process.exit()
    added process.exit() to hard exit out of event loop once request is read

--- code/01-creating-a-node-server/app.js
+++ code/01-creating-a-node-server/app.js
@@ -2,6 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+
+  /* The process core module provides a handy method that allows you to programmatically exit from a Node.js program */
+  process.exit();
 });
 
 server.listen(3000);
