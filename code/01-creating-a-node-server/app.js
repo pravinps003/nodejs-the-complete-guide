8610b7d5db0eaacd951b89d0b7398f43a8b81f59
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 21:19:02 +0530

    sending response back to user

--- code/01-creating-a-node-server/app.js
+++ code/01-creating-a-node-server/app.js
@@ -3,6 +3,15 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
 
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+
+  /* Check out more for Request and Response Headers: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers */
+
   /* The process core module provides a handy method that allows you to programmatically exit from a Node.js program */
   // process.exit();
 });
