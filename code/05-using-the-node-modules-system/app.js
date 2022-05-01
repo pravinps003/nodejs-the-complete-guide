cd66ea683253e4edc309e3c0d1f51e3c9cba6e2d
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-02 00:55:09 +0530

    missed import after updating exports

--- code/05-using-the-node-modules-system/app.js
+++ code/05-using-the-node-modules-system/app.js
@@ -4,6 +4,6 @@ const routes = require('./routes');
 
 console.log(routes.someText);
 
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
