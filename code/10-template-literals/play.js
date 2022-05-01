fbba7b2900ec8dbd63016b2759b4a9c8d394bdcd
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-01 18:34:10 +0530

    understanding template literals for string operations

---
+++ code/10-template-literals/play.js
@@ -0,0 +1,9 @@
+console.log('A String'); // Also double quotes
+console.log(`Another way of writing strings`);
+
+const name = 'PS003';
+const age = 29;
+
+console.log('My name is ' + name + ' and I am ' + age + ' years old.');
+/* Using template literals */
+console.log(`My name is ${name} and I am ${age} years old.`);
