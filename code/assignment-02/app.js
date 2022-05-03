ae0ac7b3da5a5c76ea1bce054aa66374595dbf60
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-03 16:13:21 +0530

    handled routes for /users and /default

--- code/assignment-02/app.js
+++ code/assignment-02/app.js
@@ -4,14 +4,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the Middleware');
-  next();
+app.use('/users', (req, res, next) => {
+  console.log('In route /users middleware');
+  res.send('<p>Users Page</p>');
 });
 
-app.use((req, res, next) => {
-  console.log('In another Middleware');
-  res.send('<h1>Hello from Express</h1>');
+app.use('/', (req, res, next) => {
+  console.log('In default / middleware');
+  res.send('<p>Home Page</p>');
 });
 
 app.listen(3000);
