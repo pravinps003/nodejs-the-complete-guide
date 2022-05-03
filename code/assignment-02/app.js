543f51ebcd4513ca5b77c53437cc88c444fe4adb
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-03 16:04:03 +0530

    created express app and added middlewares

---
+++ code/assignment-02/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the Middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another Middleware');
+  res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);
