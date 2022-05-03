dd0b12cdd5f35ba969f4c2e502ed7ab860e680c9
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2022-05-03 15:21:38 +0530

    use next to switch to another middleware
    Allows the request to continue to the next middleware in line

--- code/01-installing-express.js/app.js
+++ code/01-installing-express.js/app.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('In the middleware!');
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
 });
 
 const server = http.createServer(app);
