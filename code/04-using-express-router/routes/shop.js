c37d877abc578b015b7ed32bd639d21c7b98ff87
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 00:14:34 +0530

    rectified irregular commits with proper HTML

--- code/04-using-express-router/routes/shop.js
+++ code/04-using-express-router/routes/shop.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  res.send(`<h1>Hello from Express</h1>`);
 });
 
 module.exports = router;
