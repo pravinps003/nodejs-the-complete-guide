87a8d46f5879cff595140fd9dd96e3631e1a239a
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 00:19:25 +0530

    added helper function for navigation

--- code/05-creating-html-pages/routes/admin.js
+++ code/05-creating-html-pages/routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // Route: /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // Route: /admin/product => POST
