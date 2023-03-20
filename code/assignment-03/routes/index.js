31b278d1df2401f07e7b40598a5cdd3549550972
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 01:41:38 +0530

    assignment solution navigation
    assignment solution involving routing and serving html files statically

---
+++ code/assignment-03/routes/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
+router.get('/users', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'users.html'));
+});
+
+module.exports = router;
