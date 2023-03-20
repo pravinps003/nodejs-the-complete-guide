2e37d8199a7002ec41f1ae16a4c48d39c22b0f29
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-20 21:22:08 +0530

    using express router
    - moved routes in separate routes file using express router

--- code/04-using-express-router/app.js
+++ code/04-using-express-router/app.js
@@ -5,26 +5,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">
-        Add Product
-      </button>
-    </form>
-  `);
-});
-
-app.post('/product', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send(`<h1>Hello from Express</h1>`);
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
