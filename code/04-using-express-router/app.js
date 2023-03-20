85df1be8c5f237fca5bbafd835995cfd482dd9a5
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-20 21:08:31 +0530

    base set for using express router

---
+++ code/04-using-express-router/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" />
+      <button type="submit">
+        Add Product
+      </button>
+    </form>
+  `);
+});
+
+app.post('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send(`<h1>Hello from Express</h1>`);
+});
+
+app.listen(3000);
