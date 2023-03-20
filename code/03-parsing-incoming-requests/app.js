797b08e898a7e08bb941dc7ebda84f069ab8710a
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-20 20:58:47 +0530

    parsing incoming requests using body-parser

--- code/03-parsing-incoming-requests/app.js
+++ code/03-parsing-incoming-requests/app.js
@@ -1,16 +1,26 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/add-product', (req, res, next) => {
-  res.send(`<h1>The "Add Product" Page</h1>`);
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" />
+      <button type="submit">
+        Add Product
+      </button>
+    </form>
+  `);
+});
+
+app.use('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
