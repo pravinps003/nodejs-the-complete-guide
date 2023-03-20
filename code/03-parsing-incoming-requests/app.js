758611e2579aed49831ee857084afc612ca137cf
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-20 20:40:02 +0530

    base set for parsing incoming requests

---
+++ code/03-parsing-incoming-requests/app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
+  next();
+});
+
+app.use('/add-product', (req, res, next) => {
+  res.send(`<h1>The "Add Product" Page</h1>`);
+});
+
+app.use('/', (req, res, next) => {
+  res.send(`<h1>Hello from Express</h1>`);
+});
+
+app.listen(3000);
