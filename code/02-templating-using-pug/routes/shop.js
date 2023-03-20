0b63bec77c6cc2a3853d7836c53fa10e30b6b945
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 03:30:34 +0530

    outputting dynamic title and templating

--- code/02-templating-using-pug/routes/shop.js
+++ code/02-templating-using-pug/routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  res.render('shop', { docTitle: 'My Shop' });
 });
 
 module.exports = router;
