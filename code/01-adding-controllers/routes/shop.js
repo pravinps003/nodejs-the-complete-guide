beedab8d48a2ec4a084687b2946349ba98904da9
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 08:32:29 +0530

    updated routes and products controller
    - added 2 methods in controller and moved products variable

--- code/01-adding-controllers/routes/shop.js
+++ code/01-adding-controllers/routes/shop.js
@@ -1,12 +1,9 @@
 const express = require('express');
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { pageTitle: 'Shop', prods: products, path: '/' });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
