751fb2a17c24bd2ea58fcf1ca15a1d397f7e73d5
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-21 05:48:22 +0530

    converted remaining HTML files using handlebars
    - used layouts with proper properties
    - updated config for layout
    - added conditions for class
    -iterated over products

--- code/03-templating-using-express-handlebars/app.js
+++ code/03-templating-using-express-handlebars/app.js
@@ -6,7 +6,14 @@ const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHandlebars());
+app.engine(
+  'hbs',
+  expressHandlebars({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
