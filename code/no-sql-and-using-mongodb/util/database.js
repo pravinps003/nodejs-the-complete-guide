21a29dabbf244b46434295a3237f951a3b66bb74
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-23 02:15:34 +0530

    configured the mongodb server to connect using mongo client
    - commented out the sequelize content

--- code/no-sql-and-using-mongodb/util/database.js
+++ code/no-sql-and-using-mongodb/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'pravin@P$003', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://ps003:mongops003@cluster0.rcwfq.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected');
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
