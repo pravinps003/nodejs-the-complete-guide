c21750e73c03257740be7c0a67c9c7eae6718ee6
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-23 03:17:37 +0530

    added changes to fetch all products

--- code/no-sql-and-using-mongodb/controllers/shop.js
+++ code/no-sql-and-using-mongodb/controllers/shop.js
@@ -1,8 +1,7 @@
 const Product = require('../models/product');
-const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then((products) => {
       res.render('shop/product-list', {
         prods: products,
@@ -41,7 +40,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then((products) => {
       res.render('shop/index', {
         prods: products,
