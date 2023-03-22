a2ed9cf03dbdade58fdeee02d6f26fe15f18726e
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-22 11:48:24 +0530

    created one to many relationship
    - created one to many relationship from User to Product

--- code/connect-to-db-using-sequelize/app.js
+++ code/connect-to-db-using-sequelize/app.js
@@ -7,6 +7,9 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,8 +26,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     app.listen(3000);
   })
