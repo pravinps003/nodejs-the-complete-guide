280a5dd002bdbc43b3a1010c60aa4ee283110fb8
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-22 11:32:59 +0530

    created User model

---
+++ code/connect-to-db-using-sequelize/models/user.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+const Sequelize = require('../util/database');
+
+const User = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+module.exports = User;
