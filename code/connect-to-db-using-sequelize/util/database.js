ae1106763b3c72d9c2114ff7b9538d961e84d2af
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-22 09:06:38 +0530

    initialized sequelize to connect to mysql db

--- code/connect-to-db-using-sequelize/util/database.js
+++ code/connect-to-db-using-sequelize/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'pravin@P$003', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'pravin@P$003',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
