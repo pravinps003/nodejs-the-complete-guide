89f9622dceb4252f5cbf6b68ab1612728fff12eb
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 04:04:22 +0530

    using the session middleware

--- code/using-sessions-and-cookies/controllers/auth.js
+++ code/using-sessions-and-cookies/controllers/auth.js
@@ -1,6 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split('=')[1];
-  req.isLoggedIn = isLoggedIn;
+  req.isLoggedIn = req.session.isLoggedIn;
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -9,6 +8,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
