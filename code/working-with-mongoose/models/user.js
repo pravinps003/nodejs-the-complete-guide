c4d7da6db8f449e956c8e5ddec764c1e0d5aa4be
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 01:54:06 +0530

    changes to create order
    - adds order with only productId

--- code/working-with-mongoose/models/user.js
+++ code/working-with-mongoose/models/user.js
@@ -102,33 +102,6 @@ module.exports = mongoose.model('User', userSchema);
 //       });
 //   }
 
-//   addOrder() {
-//     const db = getDb();
-//     return this.getCart()
-//       .then((products) => {
-//         const order = {
-//           items: products,
-//           user: {
-//             _id: new ObjectId(this._id),
-//             name: this.name,
-//           },
-//         };
-//         return db.collection('orders').insertOne(order);
-//       })
-//       .then((result) => {
-//         this.cart = { items: [] };
-//         return db
-//           .collection('users')
-//           .updateOne(
-//             { _id: new ObjectId(this._id) },
-//             { $set: { cart: { items: [] } } }
-//           );
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-
 //   getOrders() {
 //     const db = getDb();
 //     return db
