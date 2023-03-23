387737d3278741949e5cc6638b2d5f7ddc285ed2
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 01:30:05 +0530

    changes to delete cart items

--- code/working-with-mongoose/models/user.js
+++ code/working-with-mongoose/models/user.js
@@ -47,6 +47,17 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
@@ -89,22 +100,6 @@ module.exports = mongoose.model('User', userSchema);
 //       .catch((err) => {
 //         console.log(err);
 //       });
-//   }
-
-//   deleteItemFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter((item) => {
-//       return item.productId.toString() !== productId.toString();
-//     });
-//     const updatedCart = {
-//       items: updatedCartItems,
-//     };
-//     const db = getDb();
-//     return db
-//       .collection('users')
-//       .updateOne(
-//         { _id: new ObjectId(this._id) },
-//         { $set: { cart: updatedCart } }
-//       );
 //   }
 
 //   addOrder() {
