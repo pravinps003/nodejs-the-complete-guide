b00cacd29edfb3bd384cefb9fbd6caf5819e72d0
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-23 08:05:12 +0530

    changes to add products to cart

--- code/no-sql-and-using-mongodb/app.js
+++ code/no-sql-and-using-mongodb/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('641ba9fd77dc83c20c0f702d')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
