71f1d73a633f69a05e11e1ea2243b757d7d17928
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 03:50:32 +0530

    initialized the session middleware

--- code/using-sessions-and-cookies/app.js
+++ code/using-sessions-and-cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -19,6 +20,9 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById('641ca16238714796b5971d6c')
