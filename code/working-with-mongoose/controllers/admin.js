4dd425ebb85f39655921f78573dcefeb2d753343
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-23 23:49:27 +0530

    changes to add product to collections
    - configured Product as object {} while creating the instance
    - uses save method given by mongoose

--- code/working-with-mongoose/controllers/admin.js
+++ code/working-with-mongoose/controllers/admin.js
@@ -10,14 +10,12 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
-  const product = new Product(
+  const product = new Product({
     title,
     price,
     description,
     imageUrl,
-    null,
-    req.user._id
-  );
+  });
   product
     .save()
     .then((result) => {
