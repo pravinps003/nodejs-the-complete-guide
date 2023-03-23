03d9e8e5a8f6f6f7df26cb199f0c1ad7eb66cae0
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-23 23:25:54 +0530

    configured mongoose in app.js

--- code/working-with-mongoose/app.js
+++ code/working-with-mongoose/app.js
@@ -2,10 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-
-const { mongoConnect } = require('./util/database');
 
 const User = require('./models/user');
 
@@ -34,6 +33,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://ps003:mongops003@cluster0.rcwfq.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
