2d80310d981c4025155b3eef62e0e514c0cc3d30
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 02:24:09 +0530

    base setup to use session and cookies

---
+++ code/using-sessions-and-cookies/app.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('641ca16238714796b5971d6c')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://ps003:mongops003@cluster0.rcwfq.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Pravin',
+          email: 'pravin@ps003.com',
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
