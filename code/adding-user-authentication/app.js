da38f1ba92ce7566fcde1f811a70302325c6b042
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 06:57:12 +0530

    removed changes that made dummy user

--- code/adding-user-authentication/app.js
+++ code/adding-user-authentication/app.js
@@ -58,16 +58,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: 'Pravin',
-          email: 'pravin@ps003.com',
-          cart: { items: [] },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => console.log(err));
