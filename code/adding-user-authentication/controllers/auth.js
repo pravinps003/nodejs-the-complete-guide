b04c4b34c20db44be67e598090cefcb9173f9d47
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 06:26:51 +0530

    base setup to add authentication
    - updated with signup page and related form

---
+++ code/adding-user-authentication/controllers/auth.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('641ca16238714796b5971d6c')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect('/');
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
+};
