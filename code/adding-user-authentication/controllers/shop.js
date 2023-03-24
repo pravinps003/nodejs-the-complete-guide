9c275596de963bc9f6f55d53e7fbe4dae9cf83ba
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-24 06:56:07 +0530

    updated User model and related data
    - updated user model to align with signup to only have email and remove name

--- code/adding-user-authentication/controllers/shop.js
+++ code/adding-user-authentication/controllers/shop.js
@@ -89,7 +89,7 @@ exports.postOrders = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user,
         },
         products,
