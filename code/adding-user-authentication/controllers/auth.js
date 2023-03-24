7c345936cccbb92945c6c973143c122d0f6b0e22
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-25 01:59:59 +0530

    handled flash error message for signup page

--- code/adding-user-authentication/controllers/auth.js
+++ code/adding-user-authentication/controllers/auth.js
@@ -17,9 +17,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message,
   });
 };
 
@@ -44,6 +51,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password.');
           res.redirect('/login');
         })
         .catch((err) => {
@@ -59,6 +67,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email })
     .then((existingUser) => {
       if (existingUser) {
+        req.flash(
+          'error',
+          'Email exists already, please pick a different one.'
+        );
         return res.redirect('/signup');
       }
       return bcrypt
