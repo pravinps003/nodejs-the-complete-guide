88c1ff8507fbd7a98f523afe4e8ff73e32e0510e
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-25 11:17:37 +0530

    proper use of error handling middleware

--- code/error-handling/app.js
+++ code/error-handling/app.js
@@ -43,6 +43,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -55,14 +61,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
