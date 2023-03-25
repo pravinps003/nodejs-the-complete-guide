0d81fd32d1c57889b8fff877f29b9e3b9ca627e5
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-25 08:29:19 +0530

    sanitized user inputs with sanitizers
    Refer https://github.com/validatorjs/validator.js#sanitizers

--- code/forms-user-input-and-validation/routes/auth.js
+++ code/forms-user-input-and-validation/routes/auth.js
@@ -13,10 +13,14 @@ router.get('/signup', authController.getSignup);
 router.post(
   '/login',
   [
-    body('email').isEmail().withMessage('Please enter a valid email address.'),
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
     body('password', 'Please enter a valid password.')
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -39,19 +43,23 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       'password',
       'Please enter a password with only numbers and text with at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body('confirmPassword').custom((confirmPassword, { req }) => {
-      if (confirmPassword !== req.body.password) {
-        throw new Error('Passwords have to match!');
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .custom((confirmPassword, { req }) => {
+        if (confirmPassword !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+      .trim(),
   ],
   authController.postSignup
 );
