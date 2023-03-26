530f962317d6f237f4852a382e4bbae7ec54fdfc
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-27 01:58:39 +0530

    manipulated the DOM
    -manipulated the DOM to delete the element holding the product that was deleted
    - handled without page refresh

--- code/asynchronous-javascript-requests/public/js/admin.js
+++ code/asynchronous-javascript-requests/public/js/admin.js
@@ -2,12 +2,20 @@ const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+  const productElement = btn.closest('article');
+
   fetch('/admin/product/' + prodId, {
     method: 'DELETE',
     headers: {
       'csrf-token': csrf,
     },
   })
-    .then((result) => console.log(result))
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      console.log('ps003', data);
+      productElement.parentNode.removeChild(productElement);
+    })
     .catch((err) => console.log(err));
 };
