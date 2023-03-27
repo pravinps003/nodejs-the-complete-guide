cb4c8a241903037b994409adc007657c340298d9
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-28 04:49:42 +0530

    served static images and added error handling

--- code/rest-api-for-social-networking/controllers/feed.js
+++ code/rest-api-for-social-networking/controllers/feed.js
@@ -20,10 +20,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed, entered data is incorrect',
-      errors: errors.array(),
-    });
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
   }
   const { title, content } = req.body;
   const post = new Post({
@@ -36,9 +35,14 @@ exports.createPost = (req, res, next) => {
     .save()
     .then((post) => {
       res.status(201).json({
-        message: 'Post created successfully',
+        message: 'Post created successfully!',
         post,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
