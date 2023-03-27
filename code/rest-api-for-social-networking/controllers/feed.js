637b2ab2d4cdd42edb45a61853143776a4254870
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-28 03:20:07 +0530

    added server side validation on create post route

--- code/rest-api-for-social-networking/controllers/feed.js
+++ code/rest-api-for-social-networking/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -14,6 +16,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect',
+      errors: errors.array(),
+    });
+  }
   const { title, content } = req.body;
   // Create Post in DB
   res.status(201).json({
