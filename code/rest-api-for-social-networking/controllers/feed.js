feae9efbb8b227615f497a8fa0a7cfb3cf4f543b
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-28 05:08:16 +0530

    fetched list of posts and single post
    - fetched posts using find
    - added route and method to fetch single post
    - added api to fetch single post and serve static image in UI

--- code/rest-api-for-social-networking/controllers/feed.js
+++ code/rest-api-for-social-networking/controllers/feed.js
@@ -3,18 +3,19 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/karma.jpg',
-        creator: { name: 'Pravin' },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: 'Fetched posts successfully.',
+        posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -36,6 +37,28 @@ exports.createPost = (req, res, next) => {
     .then((post) => {
       res.status(201).json({
         message: 'Post created successfully!',
+        post,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const { postId } = req.params;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: 'Post fetched',
         post,
       });
     })
