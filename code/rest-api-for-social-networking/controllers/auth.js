3b6e087701badc9f38d478537a5128a665f9025a
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-29 01:44:24 +0530

    registered a login route and method
    - handled user check
    - handled password compare

--- code/rest-api-for-social-networking/controllers/auth.js
+++ code/rest-api-for-social-networking/controllers/auth.js
@@ -31,3 +31,31 @@ exports.signup = (req, res, next) => {
       next(err);
     });
 };
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((passwordMatch) => {
+      if (!passwordMatch) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- code/rest-api-for-social-networking/routes/auth.js
+++ code/rest-api-for-social-networking/routes/auth.js
@@ -26,4 +26,6 @@ router.put(
   authController.signup
 );
 
+router.post('/login');
+
 module.exports = router;
