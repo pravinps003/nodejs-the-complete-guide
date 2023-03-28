6bfbf1b99ae9a9d9ea58939d5533b91b19620ea4
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-29 03:03:01 +0530

    cleared post-user relation after delete
    - deleted postId from user once post is deleted

--- code/rest-api-for-social-networking/controllers/feed.js
+++ code/rest-api-for-social-networking/controllers/feed.js
@@ -174,6 +174,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((user) => {
       res.status(200).json({ message: 'Deleted post.' });
     })
     .catch((err) => {
