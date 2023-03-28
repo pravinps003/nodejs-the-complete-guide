7af770a4e816f386ef1ba9197b040950f749ba97
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-29 02:04:01 +0530

    logged in and created a JWT
    - created a JSON Web Token using jwt.sign
    - added login controller method to it's route
    - adjusted UI to handle login by adding login API and passing method, headers and body params

--- code/rest-api-for-social-networking/controllers/auth.js
+++ code/rest-api-for-social-networking/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -51,6 +52,18 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error;
       }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        'somesuperdupersecret',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token,
+        userId: loadedUser._id.toString(),
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
