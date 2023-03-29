6d343a6881f033653ffab573531d45a9017a21a3
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-29 06:16:24 +0530

    configured socket.io to share instance across files
    - configured in socket.js
    - added credentials: true for cors as a safety measure

--- code/rest-api-for-social-networking/app.js
+++ code/rest-api-for-social-networking/app.js
@@ -58,12 +58,7 @@ mongoose
   )
   .then((result) => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server, {
-      cors: {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-      },
-    });
+    const io = require('./socket').init(server);
     io.on('connection', (socket) => {
       console.log('Client connected');
     });
