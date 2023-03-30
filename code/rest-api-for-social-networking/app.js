e6d79f21fe84a92a8443108edf1e2be613570825
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-30 05:53:49 +0530

    removed routes from server
    - since we'll be using graphql apis

--- code/rest-api-for-social-networking/app.js
+++ code/rest-api-for-social-networking/app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -40,9 +37,6 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
