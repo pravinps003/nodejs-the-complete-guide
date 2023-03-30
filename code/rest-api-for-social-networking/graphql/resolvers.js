55f484c9ad8340f001a555180207ed45ac2309fd
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-30 07:37:23 +0530

    added input validation using validator
    - used default on validator to provide intellisense

--- code/rest-api-for-social-networking/graphql/resolvers.js
+++ code/rest-api-for-social-networking/graphql/resolvers.js
@@ -1,9 +1,25 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator').default;
+
 const User = require('../models/user');
 
 module.exports = {
   createUser: async ({ userInput }, req) => {
     const { email, name, password } = userInput;
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'Email is invalid.' });
+    }
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short!' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      throw error;
+    }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error('User exists already!');
