c73d1a58ede02bb1076e899e41b749605c224f8d
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-30 06:29:00 +0530

    configured graphql schema and resolver in app with dummy test data
    - testing graphql api with dummy data
    POST: http://localhost:8080/graphql
    JSON Body:
    {
        "query": "{ hello { text } }"
    }
    OR
    {
        "query": "{ hello { text views } }"
    }

--- code/rest-api-for-social-networking/app.js
+++ code/rest-api-for-social-networking/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const { graphqlHTTP } = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 
@@ -37,6 +41,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
