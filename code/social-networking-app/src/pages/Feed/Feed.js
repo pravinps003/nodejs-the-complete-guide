4ba7924ddee321700a4c602f1b58657572b4d59d
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-30 12:06:07 +0530

    added logic to display created posts

--- code/social-networking-app/src/pages/Feed/Feed.js
+++ code/social-networking-app/src/pages/Feed/Feed.js
@@ -200,7 +200,17 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt,
         };
         this.setState((prevState) => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              (p) => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
