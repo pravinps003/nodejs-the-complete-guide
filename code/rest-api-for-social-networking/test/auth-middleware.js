3e5d794c3795c3275ba8b608ac91ff92a8db2529
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-31 10:05:45 +0530

    organized multiple tests under a group using describe
    - organized multiple tests under a group using describe
    - Takes a proper header to describe the group of tests under it
    - describe can also have describe and it...

--- code/rest-api-for-social-networking/test/auth-middleware.js
+++ code/rest-api-for-social-networking/test/auth-middleware.js
@@ -2,11 +2,20 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', () => {
-  const req = {
-    get: (headerName) => null,
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    'Not authenticated.'
-  );
+describe('Auth Middleware', () => {
+  it('should throw an error if no authorization header is present', () => {
+    const req = {
+      get: (headerName) => null,
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', () => {
+    const req = {
+      get: (headerName) => 'xyz',
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
