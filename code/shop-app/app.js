b1489e58aa3434cb29089e4be3ac942678cc326c
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-31 06:23:44 +0530

    configured morgan for request logging
    - configured morgan with 'combined' format to write/stream logs to access.log file with flag 'a' meaning append next requests
    - also attached samples in access.log for combined format
    Refer: https://github.com/expressjs/morgan#predefined-formats and more for details

--- code/shop-app/app.js
+++ code/shop-app/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -10,6 +11,7 @@ const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 
@@ -48,8 +50,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
 app.use(helmet());
 app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
