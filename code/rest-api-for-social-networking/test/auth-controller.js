ba2df9c2f8007510c5acddc4733c2033fd882867
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-03-31 12:51:36 +0530

    set up a testing database
    - set up a testing database viz. 'test-messages'
    - created a dummy user

--- code/rest-api-for-social-networking/test/auth-controller.js
+++ code/rest-api-for-social-networking/test/auth-controller.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
@@ -23,5 +24,23 @@ describe('Auth Controller - Login', () => {
     });
 
     User.findOne.restore();
+  });
+
+  it('should send a response with a valid user status for an existin user', (done) => {
+    mongoose
+      .connect(
+        'mongodb+srv://ps003:mongops003@cluster0.rcwfq.mongodb.net/test-messages?retryWrites=true&w=majority'
+      )
+      .then((result) => {
+        const user = new User({
+          email: 'ps003@ps003.com',
+          password: 'ps003',
+          name: 'PS003',
+          posts: [],
+        });
+        return user.save();
+      })
+      .then(() => {})
+      .catch((err) => console.log(err));
   });
 });
