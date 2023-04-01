b380eb57ac97f99f66a77fe1e9bc2e3384f866b4
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-01 15:15:40 +0530

    updated code from deno doc
    returns user agent in body on running 'deno run --allow-net app.ts'

--- code/03-basic-deno-server/app.ts
+++ code/03-basic-deno-server/app.ts
@@ -1,3 +1,14 @@
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
-serve(() => new Response("Hello World\n"), { port: 3000 });
+const port = 3000;
+
+const handler = (request: Request): Response => {
+  const body = `Your user-agent is:\n\n${
+    request.headers.get("user-agent") ?? "Unknown"
+  }`;
+
+  return new Response(body, { status: 200 });
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:3000/`);
+await serve(handler, { port });
