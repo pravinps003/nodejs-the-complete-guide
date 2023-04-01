b0c92cc3efa2c37fdf13efc826cced3b7279d4bb
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-01 06:26:22 +0530

    rebuilt __dirname for es modules
    - used path, url to frame dirname

--- code/modern-javascript-and-nodejs/response-handler.js
+++ code/modern-javascript-and-nodejs/response-handler.js
@@ -1,7 +1,10 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  fs.readFile('my-page.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
+  res.sendFile(path.join(__dirname, 'my-page.html'));
 };
